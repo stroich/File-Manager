b57ed3955227eb545c44a7190bae5a30e30bf1ef
stroich <stroichmwork@gmail.com>
2024-01-31 20:11:23 +0400

    fix: fix the transition from the root directory

--- src/navigation/goUpper.js
+++ src/navigation/goUpper.js
@@ -1,10 +1,18 @@
 import path from 'path';
+import { getCurrentDirectory } from './getCurrentDirectory.js';
 
 export function goUpper() {
     try {
         const cwd = process.cwd();
-        const parentDirectory = path.resolve(cwd, '..');
-        process.chdir(parentDirectory);
+        const root = path.parse(cwd).root;
+        if (cwd === root) {
+            console.log('Operation failed: root directory');
+        } else {
+            const parentDirectory = path.resolve(cwd, '..');
+            process.chdir(parentDirectory);
+            getCurrentDirectory();
+        }
+
     } catch {
         console.log('Operation failed');
     }

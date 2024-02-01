21a7e8613cfbeb6847addbd4b7f7e14c60f84342
stroich <stroichmwork@gmail.com>
2024-02-01 19:04:12 +0400

    fix: fix the add command

--- src/basicOperations/add.js
+++ src/basicOperations/add.js
@@ -1,11 +1,13 @@
 import fs from 'fs/promises';
 import path from 'path';
+import { getCurrentDirectory } from '../navigation/index.js';
 
 export async function add(file) {
     try {
         const cwd = process.cwd();
-        const pathNewFile = path.join(cwd, file);
+        const pathNewFile = path.resolve(cwd, file);
         await fs.writeFile(pathNewFile, '');
+        getCurrentDirectory();
     } catch {
         console.log('Operation failed');
     }
